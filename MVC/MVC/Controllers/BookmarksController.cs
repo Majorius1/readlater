b18fd36fdd12330084740654228d36def04f3b1d
Bohdan Shchur <bogdanshur1@gmail.com>
2020-08-23 16:42:56 +0300

    Implement "Create bookmark" functionality with possibility to create category during this stage

---
+++ MVC/MVC/Controllers/BookmarksController.cs
@@ -0,0 +1,52 @@
+﻿using ReadLater.Entities;
+using ReadLater.Services;
+using System.Collections.Generic;
+using System.Net;
+using System.Web.Mvc;
+
+namespace MVC.Controllers {
+	public class BookmarksController : Controller {
+		private readonly ICategoryService _categoryService;
+		private readonly IBookmarkService _bookmarkService;
+
+		public BookmarksController(ICategoryService categoryService, IBookmarkService bookmarkService) {
+			_categoryService = categoryService;
+			_bookmarkService = bookmarkService;
+		}
+
+		private void InitializaCategoriesSelectList() {
+			var categories = _categoryService.GetCategories();
+			var categoriesSelectList = new SelectList(categories, "ID", "Name");
+			ViewBag.CategoriesSelectList = categoriesSelectList;
+		}
+
+		[HttpGet]
+		public ActionResult Create() {
+			InitializaCategoriesSelectList();
+			return View();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Create(Bookmark bookmark) {
+			if (ModelState.IsValid) {
+				_bookmarkService.CreateBookmark(bookmark);
+				return RedirectToAction("Index");
+			}
+
+			return View(bookmark);
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult CreateCategoryWithoutReload(Category category) {
+			if (ModelState.IsValid) {
+				_categoryService.CreateCategory(category);
+				var categories = _categoryService.GetCategories();
+				return PartialView("CategoriesDropdown", new SelectList(categories, "ID", "Name"));
+			}
+
+			return View(category);
+		}
+	}
+}
---
+++ MVC/MVC/Scripts/bookmarks.js
@@ -0,0 +1,11 @@
+﻿$(".category-create").on("click", function (event) {
+	event.preventDefault();
+
+	$(".create-category-form").show();
+	$(this).hide();
+});
+
+function hideForm() {
+	$(".create-category-form").hide();
+	$(".category-create").show();
+}
