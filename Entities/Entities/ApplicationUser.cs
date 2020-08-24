3a275283f93343054c03b9f895991d7c75fa052e
Bohdan Shchur <bogdanshur1@gmail.com>
2020-08-25 02:21:40 +0300

    Make ApplicationUser inherit from IObjectState in order to update service to work

--- Entities/Entities/ApplicationUser.cs
+++ Entities/Entities/ApplicationUser.cs
@@ -1,14 +1,18 @@
 ﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace ReadLater.Entities {
-	public class ApplicationUser : IdentityUser {
+	public class ApplicationUser : IdentityUser, IObjectState {
 		public virtual ICollection<Category> Categories { get; set; }
 
 		public virtual ICollection<Bookmark> Bookmarks { get; set; }
+
+		[NotMapped]
+		public ObjectState ObjectState { get; set; }
 
 		public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager) {
 			// Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
